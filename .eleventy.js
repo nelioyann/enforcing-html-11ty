f70bb8ad6f98714a1cffc73b0aaa9347153a317b
nelioyann <nanayannick1998@gmail.com>
2022-11-30 02:03:56 +0100

    feat: ✨ Implement Evaluatory configuration

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addNunjucksFilter("enforce", function(value) {
+        if(value === undefined){
+            throw "A parameter is missing"
+        }
+        return(value);
+    });
+
     return {
         dir: {
             input: "src"
---
+++ src/_data/site.js
@@ -0,0 +1,13 @@
+module.exports = {
+title: "HTML Practices",
+description: "Enforcing HTML best practices in Eleventy",
+baseUrl: (process.env.ELEVENTY_ENV || "").trim() === 'development' ? 'http://localhost:8080' : 'https://enforcehtml.yannicknana.fr',
+language: "en",
+author: {
+  name: "Yannick",
+  email: "hello@yannicknana.fr",
+  url: "https://yannicknana.fr/",
+  twitter: "nelioyann",
+  facebook: "nelioyann"
+  }
+}
