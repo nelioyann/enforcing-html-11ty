79b2c5f526d50c8af56476c1be4602420a37e9f4
nelioyann <nanayannick1998@gmail.com>
2022-12-13 22:25:04 +0100

    feat: ✨ Allow advanced usage of enforced filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
 module.exports = function (eleventyConfig) {
 
-    eleventyConfig.addNunjucksFilter("enforced", function(value) {
-        if(value === undefined){
-            throw "A parameter is missing"
+    eleventyConfig.addNunjucksFilter("enforced", function(value, force) {
+        if(value === undefined ){
+            throw new Error("A parameter is missing") 
+        }
+        if( force === true ){
+            throw new Error(`An error occured: ${value}`);
         }
         return(value);
     });
